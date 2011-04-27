2bc2141cb3bbd1c5b497b7a59ada8fd4db72a9aa
tim-r <tim-r@043453e6-25df-4c13-81b0-d6a77d00cc6f>
2011-04-27 15:07:06 +0000

    XML-Dateien und SOAP-Datei zum speichern in MySQL erstellt
    git-svn-id: https://openseamap.svn.sourceforge.net/svnroot/openseamap/tidal-scale@351 043453e6-25df-4c13-81b0-d6a77d00cc6f

--- experimental/web/index.php
+++ experimental/web/index.php
@@ -1,6 +1,6 @@
 <!--
 erstellt von Tim Reinartz im Rahmen der Bachelor-Thesis
-letzte Änderung 24.04.11 11:24 Uhr
+letzte Änderung 27.04.11 17:04 Uhr
 -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <html>
@@ -27,19 +27,19 @@ letzte Änderung 24.04.11 11:24 Uhr
       <tr>
         <td><a href="xml_wget.php" target="_blank">xml_wget.php</a></td>
         <td><a href="xml_pegel_anzeigen_lokal.php" target="_blank">xml_pegel_anzeigen_lokal.php</a></td>
-        <td>&nbsp;</td>
+        <td><a href="xml_pegel_sql.php" target="_blank">xml_pegel_sql.php</a></td>
       </tr>
       <tr>
         <td>&nbsp;</td>
         <td><a href="xml_pegel_anzeigen_entfernt.php" target="_blank">xml_pegel_anzeigen_entfernt.php</a></td>
-        <td>&nbsp;</td>
+        <td><a href="xml_pegel_sql_entfernt.php" target="_blank">xml_pegel_sql_entfernt.php</a></td>
       </tr>
       <tr>
         <td>&nbsp;</td>
         <td><a href="xml_pegel_fsock_anzeigen.php" target="_blank">xml_pegel_fsock_anzeigen.php</a></td>
-        <td>&nbsp;</td>
+        <td><a href="xml_pegel_fsock_sql.php" target="_blank">xml_pegel_fsock_sql.php</a></td>
       </tr>
-	  <tr>
+      <tr>
         <td>SOAP Daten </td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
@@ -47,7 +47,7 @@ letzte Änderung 24.04.11 11:24 Uhr
       <tr>
         <td><a href="soap_pegelxml_datei.php" target="_blank">soap_pegelxml_datei.php</a></td>
         <td><a href="soap_pegelxml_datei_anzeigen.php" target="_blank">soap_pegelxml_datei_anzeigen.php</a></td>
-        <td>&nbsp;</td>
+        <td><a href="soap_pegelxml_datei_sql.php" target="_blank">soap_pegelxml_datei_sql.php</a></td>
       </tr>
     </table>
 	<br><br>
---
+++ experimental/web/xml_pegel_sql.php
@@ -0,0 +1,86 @@
+<!--
+erstellt von Tim Reinartz im Rahmen der Bachelor-Thesis
+letzte Änderung 27.04.11 15:23 Uhr
+-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html>
+    <head>
+        <title>XML-Dateien anzeigen und in mysql speichern</title>
+    </head>
+    <body>
+<?php
+include('config.inc.php');
+	//lokal z.b. mit wget geholt
+    
+    if(file_exists($xmlname)) {
+        $xml = simplexml_load_file($xmlname);
+        if($xml) { //pruefen ob gueltiges xml bzw. wohlgeformt
+		echo 'XML Datei ist Fehlerfrei bzw. Wohlgeformt wird verarbeitet ...';
+		//ins logfile schreiben
+		$msg = "XML Datei ist Fehlerfrei bzw. Wohlgeformt ausgabe ...";
+		Log::write(LOG_XML, $msg);
+		foreach ($xml->table->gewaesser as $gewaesser) {
+		echo '<h2>Gewaesser: ' . $gewaesser->name . '</h2>' . PHP_EOL;
+            foreach($gewaesser->item as $item) {
+            echo '<h4>Item: ' . $item->no . '</h4>' . PHP_EOL;
+            ?>
+        <table>
+            <tr>
+                <td>psmgr:</td>
+                <td><?php Util::echo_wert($item->psmgr); ?></td>
+            </tr>
+			<tr>
+                <td>pegelname:</td>
+                <td><?php Util::echo_wert($item->pegelname); ?></td>
+            </tr>
+            <tr>
+                <td>messwert:</td>
+                <td><?php Util::echo_wert($item->messwert); ?></td>
+            </tr>
+			<tr>
+                <td>km:</td>
+                <td><?php Util::echo_wert($item->km); ?></td>
+            </tr>
+            <tr>
+                <td>pnp:</td>
+                <td><?php Util::echo_wert($item->pnp); ?></td>
+            </tr>
+			<tr>
+                <td>tendenz:</td>
+                <td><?php Util::echo_wert($item->tendenz); ?></td>
+            </tr>
+            <tr>
+                <td>datum:</td>
+                <td><?php Util::echo_wert($item->datum); ?></td>
+            </tr>
+			<tr>
+                <td>uhrzeit:</td>
+                <td><?php Util::echo_wert($item->uhrzeit); ?></td>
+            </tr>
+            <tr>
+                <td>pegelnummer:</td>
+                <td><?php Util::echo_wert($item->pegelnummer); ?></td>
+            </tr>
+        </table>
+<?php	
+//einzelne werte an klasse übergeben
+Daten::save_update_xml($item->pegelnummer, $item->pegelname, $item->km, $item->messwert, $item->datum, $item->uhrzeit, $item->pnp, $item->tendenz);
+            }
+		}
+        } else {
+            echo '<p>Die Datei '. $xmlname .' enhaelt fehler</p>';
+			//ins logfile schreiben
+			$msg = "Die Datei '. $xmlname .' enhaelt fehler";
+			Log::write(LOG_XML, $msg);
+        }
+    } else {
+		echo '<p>Die Datei '. $xmlname .' ist nicht vorhanden</p>';
+		//ins logfile schreiben
+		$msg = "Die Datei '. $xmlname .' ist nicht vorhanden";
+		Log::write(LOG_XML, $msg);
+	}
+?>
+<br><br>
+<?=$db->getQueryCount()?> Datenbankabfragen in <?=substr($db->getQueryTimeSum(),0,6)?> Sekunden.
+    </body>
+</html>
